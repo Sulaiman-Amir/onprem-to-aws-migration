55dfbc5403dbe87b29e47ffa2b325c10b55a0887
Sulaiman Amir <SulleMaan007@gmail.com>
2025-07-16 21:13:02 +0500

    mock AWS Migration Hub simulation setup

--- modules/migrationhub/main.tf
+++ modules/migrationhub/main.tf
@@ -1,1 +1,10 @@
+resource "null_resource" "simulate_migration_hub" {
+  provisioner "local-exec" {
+    command = "echo 'Simulating AWS Migration Hub setup...'"
+  }
+
+  triggers = {
+    always_run = timestamp()
+  }
+}
 
