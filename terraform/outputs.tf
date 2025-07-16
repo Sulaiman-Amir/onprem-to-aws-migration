62d2f346960dfc7e2f089ebbe8826f9bf93a03ef
Sulaiman Amir <SulleMaan007@gmail.com>
2025-07-16 20:44:09 +0500

    define outputs for migration simulation

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,15 @@
+output "vpc_id" {
+  description = "ID of the VPC created"
+  value       = module.network.vpc_id
+}
+
+output "ec2_instance_ids" {
+  description = "IDs of EC2 instances launched"
+  value       = module.compute.instance_ids
+}
+
+output "rds_endpoint" {
+  description = "Endpoint of the target RDS database"
+  value       = module.rds.db_endpoint
+}
 
