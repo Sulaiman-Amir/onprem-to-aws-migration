b5bbba9038fe964a971dbf74680df05ca4dedd6b
Sulaiman Amir <SulleMaan007@gmail.com>
2025-07-16 21:12:22 +0500

    provision RDS instance for migration target

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,1 +1,17 @@
+resource "aws_db_instance" "migration_db" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  name                 = "migrationdb"
+  username             = "admin"
+  password             = "Migration123!"
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+  db_subnet_group_name = var.db_subnet_group_name
+
+  tags = {
+    Name = "migration-rds"
+  }
+}
 
