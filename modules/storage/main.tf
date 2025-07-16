bbb43a1aa801cf53f2e52e1b07fb8535a83bb23f
Sulaiman Amir <SulleMaan007@gmail.com>
2025-07-16 21:10:30 +0500

    create S3 buckets for migration artifacts

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,1 +1,12 @@
+resource "aws_s3_bucket" "migration_bucket" {
+  bucket = "onprem-migration-artifacts-${random_id.bucket_id.hex}"
+  force_destroy = true
+  tags = {
+    Name = "migration-artifacts"
+  }
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
 
